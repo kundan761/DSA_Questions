214dc37c29dd6376d763f8de36d36efcac60528b
kundan761 <kundankumarsuraj761@gmail.com>
2025-01-15 23:03:33 +0530

    Add StringPermutation class to generate permutations of a string

---
+++ StringPermutation.java
@@ -0,0 +1,16 @@
+public class StringPermutation {
+    public static void permutation(String str, String prefix){
+        if(str.length() == 0){
+            System.out.println(prefix);
+        }else{
+            for (int i = 0; i < str.length(); i++) {
+                String rem = str.substring(0, i) + str.substring( i + 1);
+                permutation(rem, prefix + str.charAt(i));
+            }
+        }
+    }
+    public static void main(String[] args) {
+        String str = "abcd";
+        permutation(str, "");
+    }
+}
